9446f75d1d41e231870dfa50bbb8505f22665947
RakeshMandal01 <rakeshmandalofficial01@gmail.com>
2025-05-05 14:51:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/auth', authRoutes)
 
+app.get('/ping',(req,res)=>{
+res.send('PONG)
+});
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
