a5a6be0c95660aa59349e52cc34974ac65a7b5f7
Rakesh Mandal <rakeshmandalofficial01@gmail.com>
2025-05-05 12:56:26 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+require('dotenv').config({ path: './Config/.env' })
+require("./Config/db")
+
+const authRoutes = require('./Routes/authRoutes')
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/auth', authRoutes)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+    
+});
